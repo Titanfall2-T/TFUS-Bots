2420f64cf209c7c01686786fb329bbff9d3e705c
TF2T <149207590+Titanfall2-T@users.noreply.github.com>
2024-01-19 11:13:39 -0600

    Update TFBS.bat

--- bot-files/TFBS.bat
+++ bot-files/TFBS.bat
@@ -2,12 +2,14 @@
 set "instances=12"
 ::This is how many seconds to wait before restarting all bots
 set "restarttime=7200"
+::This is the path to the game installation location
+set "path=C:\Program Files (x86)\Steam\steamapps\common\Titanfall2"
 
 echo Launching bots...
 :start
 @echo off
 FOR %%a IN (1,1,%instances%) DO (
-  start "C:\Program Files (x86)\Steam\steamapps\common\Titanfall2" NorthstarLauncher.exe -norestrictservercommands -profile=R2Titanfall +ns_has_agreed_to_send_token 2
+  start %path% NorthstarLauncher.exe -norestrictservercommands -profile=R2Titanfall +ns_has_agreed_to_send_token 2
   timeout 5
 )
 echo Starting timer...
