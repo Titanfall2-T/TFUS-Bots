daf773f0fa8479fede79039fecd6e341cd61c905
TF2T <149207590+Titanfall2-T@users.noreply.github.com>
2024-01-19 21:42:20 -0600

    Update TFBS.bat

--- bot-files/TFBS.bat
+++ bot-files/TFBS.bat
@@ -1,14 +1,34 @@
 echo Launching bots...
 :b
 @echo off
-FOR %%a IN (1,1,12) DO (
-  start "C:\Program Files (x86)\Steam\steamapps\common\Titanfall2" NorthstarLauncher.exe -norestrictservercommands -profile=R2Titanfall +ns_has_agreed_to_send_token 2
-  timeout 5
-)
+start "C:\Program Files (x86)\Steam\steamapps\common\Titanfall2" NorthstarLauncher.exe -norestrictservercommands -profile=R2Titanfall +ns_has_agreed_to_send_token 2
+timeout 5
+start "C:\Program Files (x86)\Steam\steamapps\common\Titanfall2" NorthstarLauncher.exe -norestrictservercommands -profile=R2Titanfall +ns_has_agreed_to_send_token 2
+timeout 5
+start "C:\Program Files (x86)\Steam\steamapps\common\Titanfall2" NorthstarLauncher.exe -norestrictservercommands -profile=R2Titanfall +ns_has_agreed_to_send_token 2
+timeout 5
+start "C:\Program Files (x86)\Steam\steamapps\common\Titanfall2" NorthstarLauncher.exe -norestrictservercommands -profile=R2Titanfall +ns_has_agreed_to_send_token 2
+timeout 5
+start "C:\Program Files (x86)\Steam\steamapps\common\Titanfall2" NorthstarLauncher.exe -norestrictservercommands -profile=R2Titanfall +ns_has_agreed_to_send_token 2
+timeout 5
+start "C:\Program Files (x86)\Steam\steamapps\common\Titanfall2" NorthstarLauncher.exe -norestrictservercommands -profile=R2Titanfall +ns_has_agreed_to_send_token 2
+timeout 5
+start "C:\Program Files (x86)\Steam\steamapps\common\Titanfall2" NorthstarLauncher.exe -norestrictservercommands -profile=R2Titanfall +ns_has_agreed_to_send_token 2
+timeout 5
+start "C:\Program Files (x86)\Steam\steamapps\common\Titanfall2" NorthstarLauncher.exe -norestrictservercommands -profile=R2Titanfall +ns_has_agreed_to_send_token 2
+timeout 5
+start "C:\Program Files (x86)\Steam\steamapps\common\Titanfall2" NorthstarLauncher.exe -norestrictservercommands -profile=R2Titanfall +ns_has_agreed_to_send_token 2
+timeout 5
+start "C:\Program Files (x86)\Steam\steamapps\common\Titanfall2" NorthstarLauncher.exe -norestrictservercommands -profile=R2Titanfall +ns_has_agreed_to_send_token 2
+timeout 5
+start "C:\Program Files (x86)\Steam\steamapps\common\Titanfall2" NorthstarLauncher.exe -norestrictservercommands -profile=R2Titanfall +ns_has_agreed_to_send_token 2
+timeout 5
+start "C:\Program Files (x86)\Steam\steamapps\common\Titanfall2" NorthstarLauncher.exe -norestrictservercommands -profile=R2Titanfall +ns_has_agreed_to_send_token 2
+timeout 5
 echo Starting timer...
 timeout 7200
 echo Closing bots...
 TASKKILL /IM NorthstarLauncher.exe /F
 timeout 5
 echo Restarting bots...
-goto b
+goto start
